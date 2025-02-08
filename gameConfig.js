eb4ebffcfd648ba71caf45ce328d1f5fe1a89f94
Didier Nsengiyumva <nsedidier@gmail.com>
2025-02-08 18:58:18 +0200

    Create gameConfig.js 658

---
+++ gameConfig.js
@@ -0,0 +1,18 @@
+// gameConfig.js
+const TOPICS = [
+  "Science",
+  "History",
+  "Geography",
+  "Entertainment",
+  "Sports",
+  "Technology"
+];
+
+const GAME_STATES = {
+  IDLE: 'IDLE',
+  TOPIC_SELECTION: 'TOPIC_SELECTION',
+  QUESTION_COUNT: 'QUESTION_COUNT',
+  WAITING_FOR_OPPONENT: 'WAITING_FOR_OPPONENT',
+  IN_GAME: 'IN_GAME',
+  GAME_OVER: 'GAME_OVER'
+};
